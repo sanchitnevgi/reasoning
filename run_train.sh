3ea8a6b68a9cac39d389564479c1b52d63144ee8
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-03-29 00:02:06 -0400

    update run_train

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=titanx-short
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
+#SBATCH --mem=16000
+#SBATCH --nodes=2
 
 TOTAL_NUM_UPDATES=20000  
 WARMUP_UPDATES=500      
@@ -9,7 +11,7 @@ MAX_TOKENS=256
 UPDATE_FREQ=4
 BART_PATH=bart.large/model.pt
 
-CUDA_VISIBLE_DEVICES=0 python train.py bin \
+CUDA_VISIBLE_DEVICES=0,1 python train.py bin \
     --restore-file $BART_PATH \
     --max-tokens $MAX_TOKENS \
     --task translation \
@@ -24,9 +26,9 @@ CUDA_VISIBLE_DEVICES=0 python train.py bin \
     --criterion label_smoothed_cross_entropy \
     --label-smoothing 0.1 \
     --dropout 0.1 --attention-dropout 0.1 \
-    --weight-decay 0.01 --optimizer adam --adam-betas "(0.9, 0.999)" --adam-eps 1e-08 \
+    --weight-decay 0.01 --optimizer adam --adam-betas "(0.9, 0.998)" --adam-eps 1e-08 \
     --clip-norm 0.1 \
     --lr-scheduler polynomial_decay --lr $LR --total-num-update $TOTAL_NUM_UPDATES --warmup-updates $WARMUP_UPDATES \
-    --fp16 --update-freq $UPDATE_FREQ \
+    --update-freq $UPDATE_FREQ \
     --skip-invalid-size-inputs-valid-test \
     --find-unused-parameters;
