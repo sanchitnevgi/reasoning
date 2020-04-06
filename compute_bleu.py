bc949b3434a3ab8ee681ef4fe352bc33e7a9c7d6
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-04-05 22:22:06 -0400

    Add bleu evaluation script

---
+++ compute_bleu.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import numpy as np
+
+from nltk.translate.bleu_score import sentence_bleu
+
+def evaluate_bleu(args):
+    source_file = args.source
+    hypo_file = args.hypo
+
+    with open(source_file) as sf, open(hypo_file) as hf:
+        refs, hypos = [], []
+        line_no = 1
+        sentence_scores = []
+
+        for ref, hypo in zip(sf.readlines(), hf.readlines()):
+            ref = ref.strip()
+            hypo = hypo.strip()
+
+            refs.append(ref)
+            hypos.append(hypo)
+
+            if line_no % 3 == 0:
+                scores = [sentence_bleu(refs, h) for h in hypos]
+                sentence_scores.append(np.mean(scores))
+
+                refs, hypos = [], []
+
+            line_no += 1
+
+    bleu_score = np.mean(sentence_scores)
+    print("BLEU score", bleu_score)
+            
+
+def main():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--source")
+    parser.add_argument("--hypo")
+
+    args = parser.parse_args()
+
+    evaluate_bleu(args)
+
+if __name__ == "__main__":
+    main()
---
+++ run_bleu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python compute_bleu.py --source=./data/val.source --hypo=./hypos/val_vanilla.hypo
