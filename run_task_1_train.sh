704657e8b2eee374f0f0f66ace86ab8b6a4ad639
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-04-12 14:36:33 -0400

    add test task

--- run_task_1_train.sh
+++ run_task_1_train.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=titanx-short
-#SBATCH --gres=gpu:2
-#SBATCH --mem=16000
-#SBATCH --nodes=2
+#SBATCH --gres=gpu:1
 
 TOTAL_NUM_UPDATES=2036  
 WARMUP_UPDATES=61      
@@ -11,10 +9,10 @@ NUM_CLASSES=2
 MAX_SENTENCES=16
 BART_PATH=bart.large/model.pt
 
-CUDA_VISIBLE_DEVICES=0,1 python train.py task-1-bin/ \
+python train.py MRPC-bin/ \
     --restore-file $BART_PATH \
     --max-sentences $MAX_SENTENCES \
-    --max-tokens 4400 \
+    --max-tokens 512 \
     --task sentence_prediction \
     --add-prev-output-tokens \
     --layernorm-embedding \
@@ -30,6 +28,7 @@ CUDA_VISIBLE_DEVICES=0,1 python train.py task-1-bin/ \
     --weight-decay 0.01 --optimizer adam --adam-betas "(0.9, 0.98)" --adam-eps 1e-08 \
     --clip-norm 0.0 \
     --lr-scheduler polynomial_decay --lr $LR --total-num-update $TOTAL_NUM_UPDATES --warmup-updates $WARMUP_UPDATES \
+    --fp16 --fp16-init-scale 4 --threshold-loss-scale 1 --fp16-scale-window 128 \
     --max-epoch 10 \
     --find-unused-parameters \
     --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric;
