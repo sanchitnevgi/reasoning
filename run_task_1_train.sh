4a839df6a54a303f1624b93d1cae85a036c8c1d6
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-04-12 12:35:44 -0400

    Add task-1 training

---
+++ run_task_1_train.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --partition=titanx-short
+#SBATCH --gres=gpu:2
+#SBATCH --mem=16000
+#SBATCH --nodes=2
+
+TOTAL_NUM_UPDATES=2036  
+WARMUP_UPDATES=61      
+LR=1e-05 
+NUM_CLASSES=2
+MAX_SENTENCES=16
+BART_PATH=bart.large/model.pt
+
+CUDA_VISIBLE_DEVICES=0,1 python train.py task-1-bin/ \
+    --restore-file $BART_PATH \
+    --max-sentences $MAX_SENTENCES \
+    --max-tokens 4400 \
+    --task sentence_prediction \
+    --add-prev-output-tokens \
+    --layernorm-embedding \
+    --share-all-embeddings \
+    --share-decoder-input-output-embed \
+    --reset-optimizer --reset-dataloader --reset-meters \
+    --required-batch-size-multiple 1 \
+    --init-token 0 \
+    --arch bart_large \
+    --criterion sentence_prediction \
+    --num-classes $NUM_CLASSES \
+    --dropout 0.1 --attention-dropout 0.1 \
+    --weight-decay 0.01 --optimizer adam --adam-betas "(0.9, 0.98)" --adam-eps 1e-08 \
+    --clip-norm 0.0 \
+    --lr-scheduler polynomial_decay --lr $LR --total-num-update $TOTAL_NUM_UPDATES --warmup-updates $WARMUP_UPDATES \
+    --max-epoch 10 \
+    --find-unused-parameters \
+    --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric;
